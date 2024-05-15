ea92b31685462834825f9bcc2789877ac612520c
lsm0506 <tkdals2317@gmail.com>
2024-05-15 22:54:39 +0900

    5강. 사칙연산 계산기의 나눗셈 테스트 작성

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,10 +1,15 @@
 package com.group.libraryapp.calculator
 
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -46,5 +51,38 @@ class CalculatorTest {
         if (calculator.number != 15) {
             throw IllegalStateException()
         }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("메시지가 다릅니다.")
+            }
+            // 테스트 성공
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다.")
     }
 }
