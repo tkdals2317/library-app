1d11a3edf6165172a4c79f78b194dff1b9142333
lsm0506 <tkdals2317@gmail.com>
2024-05-15 22:48:31 +0900

    Calculator 테스트 코드 작성

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -3,6 +3,8 @@ package com.group.libraryapp.calculator
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
 }
 
 class CalculatorTest {
@@ -16,6 +18,32 @@ class CalculatorTest {
 
         // then
         if (calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
             throw IllegalStateException()
         }
     }
