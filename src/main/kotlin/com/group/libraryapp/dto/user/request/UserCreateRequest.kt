fa5e3825b7e00aa14f64d9a1c196caf183fc439b
lsm0506 <tkdals2317@gmail.com>
2024-05-19 21:44:27 +0900

    18강. DTO를 Kotlin으로 변경하기

--- src/main/kotlin/com/group/libraryapp/dto/user/request/UserCreateRequest.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/request/UserCreateRequest.kt
@@ -1,20 +1,6 @@
-package com.group.libraryapp.dto.user.request;
+package com.group.libraryapp.dto.user.request
 
-public class UserCreateRequest {
-
-  private String name;
-  private Integer age;
-
-  public String getName() {
-    return name;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public UserCreateRequest(String name, Integer age) {
-    this.name = name;
-    this.age = age;
-  }
-}
+data class UserCreateRequest(
+    val name: String,
+    val age: Int?
+)
